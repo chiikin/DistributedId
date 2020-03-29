098a870b66ca27c70d8cfdcac66e492a0d405eb3
chiikin <yangdear@hotmail.com>
2020-03-29 22:58:41 +0800

    .

--- src/DistributedId.Web/Repositories/SequenceIdRepository.cs
+++ src/DistributedId.Web/Repositories/SequenceIdRepository.cs
@@ -22,7 +22,7 @@ namespace DistributedId.Web.Repositories
         public async Task<SequencedIdBuffer> GetBufferAsync(string businessId)
         {
             SequencedIdEntity sequencedId = null;
-            using (var trans = await _idStoreContext.Database.BeginTransactionAsync(System.Data.IsolationLevel.Serializable))
+            using (var trans = await _idStoreContext.Database.BeginTransactionAsync(System.Data.IsolationLevel.RepeatableRead))
             {
                 sequencedId = _idStoreContext.SequencedIds.FirstOrDefault(x => x.BusinessId == businessId);
                 if (sequencedId == null)
